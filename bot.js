bb811c9221d798c39fdb5201d276bc4b879c133b
root <root@vps515043.ovh.net>
2018-06-06 12:23:38 +0200

    modified:   bot.js 	modified:   package-lock.json 	commands/ 	help.txt 	infoserver.js 	node_modules/.bin/ 	node_modules/ajv/ 	node_modules/asn1/ 	node_modules/assert-plus/ 	node_modules/async/ 	node_modules/asynckit/ 	node_modules/aws-sign2/ 	node_modules/aws4/ 	node_modules/bcrypt-pbkdf/ 	node_modules/bluebird/ 	node_modules/caseless/ 	node_modules/co/ 	node_modules/combined-stream/ 	node_modules/core-util-is/ 	node_modules/dashdash/ 	node_modules/delayed-stream/ 	node_modules/dom-walk/ 	node_modules/ecc-jsbn/ 	node_modules/extend/ 	node_modules/extsprintf/ 	node_modules/fast-deep-equal/ 	node_modules/fast-json-stable-stringify/ 	node_modules/ffmpeg/ 	node_modules/fluent-ffmpeg/ 	node_modules/for-each/ 	node_modules/forever-agent/ 	node_modules/form-data/ 	node_modules/getpass/ 	node_modules/global/ 	node_modules/har-schema/ 	node_modules/har-validator/ 	node_modules/http-signature/ 	node_modules/inherits/ 	node_modules/is-callable/ 	node_modules/is-function/ 	node_modules/is-typedarray/ 	node_modules/isarray/ 	node_modules/isexe/ 	node_modules/isstream/ 	node_modules/jsbn/ 	node_modules/json-schema-traverse/ 	node_modules/json-schema/ 	node_modules/json-stringify-safe/ 	node_modules/jsprim/ 	node_modules/lodash/ 	node_modules/mime-db/ 	node_modules/mime-types/ 	node_modules/min-document/ 	node_modules/oauth-sign/ 	node_modules/opusscript/ 	node_modules/parse-headers/ 	node_modules/performance-now/ 	node_modules/process-nextick-args/ 	node_modules/process/ 	node_modules/punycode/ 	node_modules/qs/ 	node_modules/readable-stream/ 	node_modules/request-promise-core/ 	node_modules/request-promise/ 	node_modules/request/ 	node_modules/safer-buffer/ 	node_modules/sshpk/ 	node_modules/stealthy-require/ 	node_modules/string_decoder/ 	node_modules/through2/ 	node_modules/tough-cookie/ 	node_modules/trim/ 	node_modules/tunnel-agent/ 	node_modules/util-deprecate/ 	node_modules/uuid/ 	node_modules/verror/ 	node_modules/when/ 	node_modules/which/ 	node_modules/xhr/ 	node_modules/xml2js/ 	node_modules/xtend/ 	node_modules/youtube-audio-stream/ 	node_modules/youtube-search/ 	oldbot.js 	tool.js

--- bot.js
+++ bot.js
@@ -2,6 +2,18 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 const YoutubeStream = require('ytdl-core')
 
+
+
+//les link au commands
+const ping = require('./commands/ping');
+const help = require('./commands/help');
+const miroir = require('./commands/miroir');
+const infoserver = require('./commands/infoserver');
+//link avec la musique
+const play = require('./commands/play');
+const stop = require('./commands/stop');
+
+
 bot.on('ready', () => {
     console.log(`Logged in as ${bot.user.tag}!`);
     bot.user.setActivity('Crée par 🍀 Jonas 🍀 ').catch(console.error);
@@ -9,13 +21,12 @@ bot.on('ready', () => {
 
 bot.on('guildMemberAdd', member => {
     member.createDM().then(channel => {
-        channel.send('Bienvenue sur ' + member.guild.name + ', ' + member.displayName);
+        channel.send('Bienvenue sur ' + member.guild.name + ', ' + member.displayName + "\n Je suis un petit bot de musique :D");
     }).catch(console.error)
     // On pourrait catch l'erreur autrement ici (l'utilisateur a peut être désactivé les MP)
 });
 
 const prefix = '$';
-
 
 function sendError(message, description) {
     message.channel.send({
@@ -26,108 +37,14 @@ function sendError(message, description) {
     });
 }
 
-// les commandes /!\
-bot.on('message', message => {
-    if (message.content[0] === prefix) {
-        let splitMessage = message.content.split(" ");
-        if (splitMessage[0] === prefix + "infoserver") {
-            var embed = new Discord.RichEmbed()
-                .setDescription("Information du Discord")
-                .addField("Nom du Discord", message.guild.name)
-                .addField("Créé le", message.guild.createdAt)
-                .addField("Tu as rejoins le", message.member.joinedAt)
-                .addField("Utilisateurs sur le Discord", message.guild.memberCount)
-                .setColor("0x0000FF")
-            message.channel.sendEmbed(embed)
-        }
-        //help
-        else if (message.content === prefix + "help") {
-            var help = new Discord.RichEmbed()
-                .setAuthor("Commandes", bot.user.avatarURL, null)
-                .setThumbnail(message.guild.iconURL)
-                .setDescription("Besoin d'aide ?")
-                .addField("$help", "Besoin d'aide vielle branche")
-                .addField("$ping", "Test ma réactivité")
-                .addField("$miroir", "Toi même")
-                .addField("$mirpion", "Lance le jeu [en cours de création]")
-                .setFooter("menu d'aide")
-            message.author.createDM().then(channel => {
-                channel.send(help)
-            });
-        }
-
-        //ping
-        else if (message.content === prefix + "ping")
-            message.channel.send("Je brâme en :`" + `${message.createdTimestamp - Date.now()}` + " ms`");
 
 
-        //miroire
-        else if (message.content === prefix + "miroir") {
-            message.channel.send('Toi même')
-            message.delete()
-                .then(msg => console.log(`Deleted message from ${msg.author.username}`))
-                .catch(console.error);
-        }
 
 
-        //Jeu
-        else if (message.content === prefix + "morpion") {
-            var partie = 1;
-            message.channel.send("Debbut de partie : ")
-            var morpion = new Discord.RichEmbed()
-                .setAuthor("Commandes", bot.user.avatarURL, null)
-                .setDescription("Morpion : \n ** :free:|:free:|:free:** \n ** :free:|:free:|:free:** \n **:free:|:free:|:free:**")
-                .setFooter("crée par Jonas")
-            message.channel.send(morpion)
-            message.react("\:arrow_up:")
-            /*while (partie = 1) {
-                if (message.content === prefix + "edit")
-                message.edit('This is my new content!')
-                partie = 0;
-            }*/
-        } else if (message.content.startsWith('$play')) {
-            // On récupère le premier channel audio du serveur
-            let voiceChannel = message.guild.channels
-                .filter(function(channel) {
-                    return channel.type === 'voice'
-                })
-                .first()
-            // On récupère les arguments de la commande
-            // il faudrait utiliser une expression régulière pour valider le lien youtube
-            let args = message.content.split(' ')
-            // On rejoint le channel audio
-            voiceChannel
-                .join()
-                .then(function(connection) {
-                    // On démarre un stream à partir de la vidéo youtube
-                    let stream = YoutubeStream(args[1])
-                    stream.on('error', function() {
-                        message.reply("Je n'ai pas réussi à lire cette vidéo :(")
-                        connection.disconnect()
-                    })
-                    // On envoie le stream au channel audio
-                    // Il faudrait ici éviter les superpositions (envoie de plusieurs vidéo en même temps)
-                    connection
-                        .playStream(stream)
-                        .on('end', function() {
-                            connection.disconnect()
-                        })
-                })
-        } else if (message.content === prefix + "stop") {
-            let voiceChannel = message.guild.channels
-                .filter(function(channel) {
-                    return channel.type === 'voice'
-                })
-                .first()
-            voiceChannel
-                .leave()
-        } else sendError(message, "Erreur, problème dans les paramètres");
-
-
-    }
+//commands
+bot.on('message', function (message) {
+  let commandUsed = ping.parse(message) || help.parse(message) || miroir.parse(message) || infoserver.parse(message) || play.parse(message) || stop.parse(message)
 })
-
-
 
 
 bot.login('NDUzNDUxODMxMzE0NjEyMjM0.DffFmQ.QHTyDQz0-7bZkMsrVdlFRbHxOe8');
